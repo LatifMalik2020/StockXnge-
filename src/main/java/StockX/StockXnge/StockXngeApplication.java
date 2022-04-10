ae32374421732a77ff141b551266f5a2064ef969
Abdul Bashir Abdul Zahir <78774688+LatifMalik2020@users.noreply.github.com>
2022-04-10 09:16:07 -0500

    Add files via upload

---
+++ src/main/java/StockX/StockXnge/StockXngeApplication.java
@@ -0,0 +1,13 @@
+package StockX.StockXnge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockXngeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockXngeApplication.class, args);
+	}
+
+}
